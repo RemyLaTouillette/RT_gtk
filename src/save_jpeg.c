c53bbdcafc174890b0c12976f86d21f3fe47feea
Sylvain DUPREY <sduprey@e1r6p9.42.fr>
2016-09-22 15:35:14 +0200

    saves op

--- src/save_jpeg.c
+++ src/save_jpeg.c
@@ -6,16 +6,56 @@
 /*   By: sduprey <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/21 19:03:02 by sduprey           #+#    #+#             */
-/*   Updated: 2016/09/21 19:04:42 by sduprey          ###   ########.fr       */
+/*   Updated: 2016/09/22 15:34:10 by sduprey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <rtv1.h>
+#include <sys/stat.h>
 
-void	save_jpeg(unsigned char *data)
+int				file_exist(char *filename)
+{
+	struct stat	buffer;
+
+	return (stat(filename, &buffer) == 0);
+}
+
+char			*filename_for_saving(void)
+{
+	char		*fname;
+	int			i;
+	int			check;
+
+	if (!(fname = (char *)malloc(sizeof(char) * 24)))
+		return (NULL);
+	fname = ft_strcpy(fname, "saves/save_img.jpeg");
+	i = 1;
+	check = file_exist(fname);
+	while (check == 1 && i < 100)
+	{
+		fname = ft_strcpy(fname, "saves/save_img(");
+		fname = ft_strcat(fname, ft_itoa(i));
+		fname = ft_strcat(fname, ").jpeg");
+		check = file_exist(fname);
+		i++;
+	}
+	return (fname);
+}
+
+void			save_jpeg(unsigned char *data)
 {
 	GdkPixbuf	*pixbuf;
+	GError		*error;
+	char		*fname;
+	int			check;
 
+	error = NULL;
+	fname = filename_for_saving();
 	pixbuf = gtk_new_image(data);
-	gdk_pixbuf_save(pixbuf, "test_img", "jpeg", NULL, "quality", "100", NULL);
+	if (fname != NULL)
+	{
+		check = gdk_pixbuf_save(pixbuf, fname, "jpeg", &error,
+				"quality", "100", NULL);
+		free(fname);
+	}
 }
